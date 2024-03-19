43581c6073ddfe3dcd188d2e8421e137350a2285
rutaborkar <33936960+rutaborkar@users.noreply.github.com>
2024-03-19 12:18:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,9 @@
 variable "instance_id" {
   description = "The ID of the EC2 instance to create an AMI from"
 }
+variable "location" {
+  type        = string
+  description = "The AWS region to use to create resources."
+  default     = "us-east-2"
+}
+
